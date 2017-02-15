5fb637a354a416058644f5921ed42936d6354229
boydchristopher7 <boydchristopher7@gmail.com>
2017-02-15 14:37:24 -0600

    script.py add

---
+++ script.py
@@ -0,0 +1,27 @@
+from math import sqrt
+import pickle
+from requests import get
+from os import path
+from numpy import sqrt, square, mean, subtract
+
+def create_cache(filename):
+    """
+    filename is the name of the cache file to load
+    returns a dictionary after loading the file or pulling the file from the public_html page
+    """
+    cache = {}
+    filePath = "/u/fares/public_html/netflix-caches/" + filename
+
+    if path.isfile(filePath):
+        with open(filePath, "rb") as f:
+            cache = pickle.load(f)
+    else:
+        webAddress = "http://www.cs.utexas.edu/users/fares/netflix-caches/" + \
+            filename
+        bytes = get(webAddress).content
+        cache = pickle.loads(bytes)
+
+    print(cache)
+
+# YEAR_OF_RATING
+create_cache("cache-yearCustomerRatedMovie.pickle")
