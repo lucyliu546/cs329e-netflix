5f1f221ce83d78d4b14958ed9fb206387077d217
boydchristopher7 <boydchristopher7@gmail.com>
2017-02-15 15:16:47 -0600

    Changes so far

--- script.py
+++ script.py
@@ -1,27 +1,10 @@
-from math import sqrt
-import pickle
-from requests import get
-from os import path
-from numpy import sqrt, square, mean, subtract
+import sys
 
-def create_cache(filename):
-    """
-    filename is the name of the cache file to load
-    returns a dictionary after loading the file or pulling the file from the public_html page
-    """
-    cache = {}
-    filePath = "/u/fares/public_html/netflix-caches/" + filename
+def truncate(reader, writer):
+    for line in reader:
+        line.strip()
+        line = line[:-13]
+        writer.write(line)
+        writer.write('\n')
 
-    if path.isfile(filePath):
-        with open(filePath, "rb") as f:
-            cache = pickle.load(f)
-    else:
-        webAddress = "http://www.cs.utexas.edu/users/fares/netflix-caches/" + \
-            filename
-        bytes = get(webAddress).content
-        cache = pickle.loads(bytes)
-
-    print(cache)
-
-# YEAR_OF_RATING
-create_cache("cache-averageMovieRating.pickle")
+truncate(sys.stdin, sys.stdout)
